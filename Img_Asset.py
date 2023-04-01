990469ba3cec971b83209b2b694ca150b8207dee
Quantum <Quantuman134@gmail.com>
2023-04-01 21:44:28 +0800

    modified:   Img_Asset.py

--- Img_Asset.py
+++ Img_Asset.py
@@ -1,7 +1,15 @@
 from PIL import Image
 from torchvision import transforms
 
+#Load an image and convert it to a tensor for torch
 class ImgTensorLoader():
     def __init__(self, image_path) -> None:
         self.img = Image.open(image_path)
-        self.img_tensor = transforms.ToTensor(self.img)
+        self.img_tensor = transforms.ToTensor()(self.img)
+
+def main():
+    ITL = ImgTensorLoader("./Assets/Images/test_image_16_16.png")
+    ITL.img.show()
+
+if __name__ == "__main__":
+    main()
